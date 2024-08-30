bcd5a9cef5f0016f6340b59df2b4b4d06984d4a6
artexhibit <vendabal@yandex.ru>
2024-08-30 21:34:46 +0300

    add data to BarChart in FragmentResult, complete project

--- FlagQuiz/app/src/main/java/ru/igorcodes/flagquiz/view/FragmentResult.kt
+++ FlagQuiz/app/src/main/java/ru/igorcodes/flagquiz/view/FragmentResult.kt
@@ -1,9 +1,15 @@
 package ru.igorcodes.flagquiz.view
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.interfaces.dataprovider.BarDataProvider
 import ru.igorcodes.flagquiz.R
 import ru.igorcodes.flagquiz.databinding.FragmentQuizBinding
 import ru.igorcodes.flagquiz.databinding.FragmentResultBinding
@@ -12,6 +18,10 @@ class FragmentResult : Fragment() {
 
     lateinit var fragmentResultBinding: FragmentResultBinding
 
+    var correctNumber = 0F
+    var emptyNumber = 0F
+    var wrongNumber = 0F
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,13 +29,45 @@ class FragmentResult : Fragment() {
 
         fragmentResultBinding = FragmentResultBinding.inflate(inflater, container, false)
 
+        val args = arguments?.let { FragmentResultArgs.fromBundle(it) }
+        args?.let {
+            correctNumber = it.correct.toFloat()
+            emptyNumber = it.empty.toFloat()
+            wrongNumber = it.wrong.toFloat()
+        }
+
+        val barEntriesArrayListCorrect = ArrayList<BarEntry>()
+        val barEntriesArrayListEmpty = ArrayList<BarEntry>()
+        val barEntriesArrayListWrong = ArrayList<BarEntry>()
+
+        barEntriesArrayListCorrect.add(BarEntry(0F, correctNumber))
+        barEntriesArrayListEmpty.add(BarEntry(1F, emptyNumber))
+        barEntriesArrayListWrong.add(BarEntry(2F, wrongNumber))
+
+        val barDataSetCorrect = BarDataSet(barEntriesArrayListCorrect, "Correct Number").apply {
+            color = Color.GREEN
+            valueTextSize = 24F
+            setValueTextColors(arrayListOf(Color.BLACK))
+        }
+        val barDataSetEmpty = BarDataSet(barEntriesArrayListEmpty, "Empty Number").apply {
+            color = Color.BLUE
+            valueTextSize = 24F
+            setValueTextColors(arrayListOf(Color.BLACK))
+        }
+        val barDataSetWrong = BarDataSet(barEntriesArrayListWrong, "Wrong Number").apply {
+            color = Color.RED
+            valueTextSize = 24F
+            setValueTextColors(arrayListOf(Color.BLACK))
+        }
+        val barData = BarData(barDataSetCorrect, barDataSetEmpty, barDataSetWrong)
+
+        fragmentResultBinding.resultChart.data = barData
+
         fragmentResultBinding.buttonNewQuiz.setOnClickListener {
-
+            this.findNavController().popBackStack(R.id.fragmentHome, false)
         }
 
-        fragmentResultBinding.buttonExit.setOnClickListener {
-
-        }
+        fragmentResultBinding.buttonExit.setOnClickListener { requireActivity().finish() }
         return fragmentResultBinding.root
     }
 }
