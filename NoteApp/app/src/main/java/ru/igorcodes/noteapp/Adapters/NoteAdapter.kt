51dacfbd411fc3420e6e38ed0efdb64f640e5358
artexhibit <vendabal@yandex.ru>
2024-09-07 19:24:02 +0300

    create UI, NoteAdapter

---
+++ NoteApp/app/src/main/java/ru/igorcodes/noteapp/Adapters/NoteAdapter.kt
@@ -0,0 +1,41 @@
+package ru.igorcodes.noteapp.Adapters
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.*
+import ru.igorcodes.noteapp.Model.Note
+import ru.igorcodes.noteapp.R
+
+class NoteAdapter: Adapter<NoteAdapter.NoteViewHolder>() {
+
+    var notes: List<Note> = ArrayList()
+
+    class NoteViewHolder(itemView: View): ViewHolder(itemView) {
+        val textViewTitle: TextView = itemView.findViewById(R.id.textViewTitle)
+        val textViewDescription: TextView = itemView.findViewById(R.id.textViewDescription)
+        val cardView: CardView = itemView.findViewById(R.id.cardView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.note_item, parent, false)
+        return  NoteViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return notes.size
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        var currentNote: Note = notes[position]
+        holder.textViewTitle.text = currentNote.title
+        holder.textViewDescription.text = currentNote.description
+    }
+
+    fun setNote(myNotes: List<Note>) {
+        this.notes = myNotes
+        notifyDataSetChanged()
+    }
+}
--- NoteApp/app/src/main/java/ru/igorcodes/noteapp/View/MainActivity.kt
+++ NoteApp/app/src/main/java/ru/igorcodes/noteapp/View/MainActivity.kt
@@ -6,6 +6,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ru.igorcodes.noteapp.Adapters.NoteAdapter
 import ru.igorcodes.noteapp.NoteApplication
 import ru.igorcodes.noteapp.R
 import ru.igorcodes.noteapp.ViewModel.NoteViewModel
@@ -24,12 +27,18 @@ class MainActivity: AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val noteAdapter = NoteAdapter()
+        recyclerView.adapter = noteAdapter
 
         val viewModelFactory = NoteViewModelFactory((application as NoteApplication).repository)
         noteViewModel = ViewModelProvider(this, viewModelFactory).get(NoteViewModel::class.java)
 
         noteViewModel.myAllNotes.observe(this, Observer { notes ->
             //updateUI
+            noteAdapter.setNote(notes)
         })
     }
 }
